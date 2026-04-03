68fb19f8d8571952932a5b9dba06b5b4b4fea1e2
zhang5120240703 <208699747@qq.com>
2026-04-03 12:14:16 +0800

    feat：更新了通信代码，python返回的字符串真正落地产生简单的action

---
+++ Assets/Scripts/Agent/TestGameState.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State
+{
+    MainMenu,
+    FirstExp,
+    SecondExp,
+    Summary
+}
+
+
+public class TestGameState : MonoBehaviour
+{
+    
+}
--- python/server.py
+++ python/server.py
@@ -8,14 +8,44 @@ app.config["JSONIFY_MIMETYPE"] = "application/json;charset=utf-8"
 @app.route("/unity",methods = ["POST"])
 def unity_api():
     data = request.get_json()
-    print("Unity发来:",data)
+    data_runState = data.get("runState")
 
-    msg_from_unity = data.get("msg","")
-    reply = f"Python已收到:{msg_from_unity}"
-    return jsonify({
-        "code":200,
-        "msg":reply
+    if not data.get("isParamValid",False):
+        return jsonify({
+            "action": "确认参数",
+            "reply": "请先确认参数"
         })
+    
+    match data_runState:
+        case "MainMenu" : 
+            return jsonify({
+                "action" : "选择实验",
+                "reply" : "在主菜单中选择下一步的实验"
+        })
+        case "FirstExp" :
+            return jsonify({
+                "action" : "实验一开始",
+                "reply" : "开始操作第一个实验"
+            })
+        case "SecondExp" : 
+            return jsonify({
+                "action" : "实验二开始",
+                "reply" : "开始操作第二个实验"
+            })
+        case "Summary" :
+            return jsonify({
+                "action" : "总结阶段",
+                "reply" : "开始进行所有实验的总结"
+            })
+    # return jsonify({
+    #     "action": "跳转到下一步",
+    #     "reply": "可以进行下一步"
+    #     })
+    #print("Unity发来:",data)
+
+    #msg_from_unity = data.get("msg","")
+    #reply = f"Python已收到:{msg_from_unity}"
+   
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 5000,debug = True)
