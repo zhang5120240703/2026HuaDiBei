05c224711b92c184f034f768a2e75ddb6dbc78a0
zhang5120240703 <208699747@qq.com>
2026-04-03 19:22:52 +0800

    修复所有的损坏文件

---
+++ Assets/Scripts/Agent/TestGameState.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State
+{
+    MainMenu,
+    FirstExp, 
+    SecondExp,
+    Summary
+}
+public class NewBehaviourScript : MonoBehaviour
+{
+    
+}
